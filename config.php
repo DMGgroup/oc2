1bd76b940af889ab61d9e7cc35f42cd274e8d7d5
Vitaliy <hodachinskiy@gmail.com>
2020-03-31 13:55:45 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc2/');
+
+// DIR
+define('DIR_APPLICATION', 'I:/OpenServer/OSPanel/domains/oc2/catalog/');
+define('DIR_SYSTEM', 'I:/OpenServer/OSPanel/domains/oc2/system/');
+define('DIR_IMAGE', 'I:/OpenServer/OSPanel/domains/oc2/image/');
+define('DIR_LANGUAGE', 'I:/OpenServer/OSPanel/domains/oc2/catalog/language/');
+define('DIR_TEMPLATE', 'I:/OpenServer/OSPanel/domains/oc2/catalog/view/theme/');
+define('DIR_CONFIG', 'I:/OpenServer/OSPanel/domains/oc2/system/config/');
+define('DIR_CACHE', 'I:/OpenServer/OSPanel/domains/oc2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'I:/OpenServer/OSPanel/domains/oc2/system/storage/download/');
+define('DIR_LOGS', 'I:/OpenServer/OSPanel/domains/oc2/system/storage/logs/');
+define('DIR_MODIFICATION', 'I:/OpenServer/OSPanel/domains/oc2/system/storage/modification/');
+define('DIR_UPLOAD', 'I:/OpenServer/OSPanel/domains/oc2/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc2elan');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocelan_');
