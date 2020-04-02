3bc259877183b7e28e1b481d8c79efade9e059bc
Vitaliy <hodachinskiy@gmail.com>
2020-04-02 03:31:22 +0300

    upd
    upd

--- catalog/view/theme/basecart/gulpfile.js
+++ catalog/view/theme/basecart/gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('styles', function() {
             overrideBrowserslist: ['last 10 versions']
         }))
         .pipe(cleancss({ level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream())
 });
 
@@ -49,44 +49,47 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
     return gulp.src([
             'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
+            'app/libs/intlTelInput/intlTelInput-jquery.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'js/_libs.js', // JS libraries (all in one)
-            'js/_custom.js', // Custom scripts. Always at the end
+            'app/libs/magnific-popup/jquery.magnific-popup.min.js',
+            'app/libs/slick/slick.min.js',
+            'app/js/_libs.js', // JS libraries (all in one)
+            'app/js/_custom.js', // Custom scripts. Always at the end
         ])
         .pipe(concat('scripts.min.js'))
         .pipe(uglify()) // Minify js (opt.)
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('app/js'))
         .pipe(browserSync.reload({ stream: true }))
 });
 
 // Responsive Images
-var quality = 95; // Responsive images quality
+var quality = 90; // Responsive images quality
 
 // Produce @1x images
 gulp.task('img-responsive-1x', async function() {
     return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-        .pipe(newer('img/@1x'))
+        .pipe(newer('app/img/@1x'))
         .pipe(responsive({
             '**/*': { width: '50%', quality: quality }
         })).on('error', function(e) { console.log(e) })
         .pipe(rename(function(path) { path.extname = path.extname.replace('jpeg', 'jpg') }))
-        .pipe(gulp.dest('img/@1x'))
+        .pipe(gulp.dest('app/img/@1x'))
 });
 // Produce @2x images
 gulp.task('img-responsive-2x', async function() {
     return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-        .pipe(newer('img/@2x'))
+        .pipe(newer('app/img/@2x'))
         .pipe(responsive({
             '**/*': { width: '100%', quality: quality }
         })).on('error', function(e) { console.log(e) })
         .pipe(rename(function(path) { path.extname = path.extname.replace('jpeg', 'jpg') }))
-        .pipe(gulp.dest('img/@2x'))
+        .pipe(gulp.dest('app/img/@2x'))
 });
 gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
 
 // Clean @*x IMG's
 gulp.task('cleanimg', function() {
-    return del(['img/@*'], { force: true })
+    return del(['app/img/@*'], { force: true })
 });
 
 // Code & Reload
